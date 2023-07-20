1e9c0cc110afa44fe4a697605d8a768c5de32df4
aghee <agymunzala@gmail.com>
2023-07-20 21:11:12 +0300

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,3 @@
 print('Hello test one correct')
 print('Now this is some -----------------')
-print('TIC-TAC-TOE GAME SAMPLE CODE--BORROWED!!!Practice work')
+print('TIC-TAC-TOE GAME SAMPLE CODE--BORROWED!!!Practice work!')
