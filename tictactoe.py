f41faf3807885140e537e6bb0093667cdbd23c37
aghee <agymunzala@gmail.com>
2023-07-21 09:19:39 +0300

    added function def ttt

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,10 @@
-print('Hello test one correct')
-print('Now this is some -----------------')
 print('TIC-TAC-TOE GAME SAMPLE CODE--BORROWED!!Practice work!!')
-#print('different again ---AGAIN')
 print('Dealt with lots of merge conflicts , now ready to start!!!')
 print('STARTING NOW HURRAY!!!!')
 #Tictactoe sample code borrowed from https://www.javatpoint.com/tic-tac-toe-in-python
+#Create function to print tictactoe design
+def ttt(val):
+    print('\n')
+    print('\t  |  |')
+    print('\t {} | {} |{}'.format(val[0],val[1],val[2]))
+    print('\t ______|______|________')
