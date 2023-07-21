b2bec34fd95cc32caf2758b5383bbe67f20dd1f8
aghee <agymunzala@gmail.com>
2023-07-21 08:35:49 +0300

    added new line

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-#print('Hello test one correct')
-#print('Now this is some -----------------')
-
-=======
-=======
-print('Heading Trial')
->>>>>>> 34ba86c7c4ac8c77a460e72192d5b87f09e8cbea
 print('Hello test one correct')
 print('Now this is some -----------------')
 print('TIC-TAC-TOE GAME SAMPLE CODE--BORROWED!!Practice work!!')
->>>>>>> bad41c4db26ee2300043b3f84a5c2bd153b47bd0
+print('different again')
